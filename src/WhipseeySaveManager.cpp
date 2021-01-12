976f42223cfe624cd4eae04c4b682da14ecccd45
StillGreen-san <40620628+StillGreen-san@users.noreply.github.com>
2021-01-12 14:38:07 +0100

    cpp cmake init

---
+++ src/WhipseeySaveManager.cpp
@@ -0,0 +1,11 @@
+/**
+ * @file WhipseeySaveManager.cpp
+ * @brief this is the main file for WhispeeySaveManager, tying all functionality together
+ */
+
+
+
+int main()
+{
+	
+}
---
+++ test/testmain.cpp
@@ -0,0 +1,7 @@
+/**
+ * @file testmain.cpp
+ * @brief gathers all tests for WhipseeySaveManager
+ */
+
+#define CATCH_CONFIG_MAIN
+#include <catch.hpp>
