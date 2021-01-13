a785265dddf68aaeb14af22152c732b62ab7e193
StillGreen-san <40620628+StillGreen-san@users.noreply.github.com>
2021-01-13 19:05:01 +0100

    raygui init

--- src/WhipseeySaveManager.cpp
+++ src/WhipseeySaveManager.cpp
@@ -3,86 +3,69 @@
  * @brief this is the main file for WhispeeySaveManager, tying all functionality together
  */
 
-#include <SimpleIni.h>
-#include <vdf_parser.hpp>
+#pragma warning(push) //disable warnings for raygui only
+#pragma warning(disable: 4244 4838 4267 4996)
 
-#include <ShlObj.h>
-#include <winreg.h>
+#define RAYGUI_IMPLEMENTATION
+#define RAYGUI_SUPPORT_ICONS
+#include <raygui.h>
 
-#include <iostream>
+#pragma warning(pop)
 
 int main()
 {
-	PWSTR ppszPath;
-	HRESULT hr = SHGetKnownFolderPath(FOLDERID_LocalAppData, KNOWN_FOLDER_FLAG::KF_FLAG_DEFAULT, NULL, &ppszPath);
-	if(hr != S_OK)
-	{
-		std::cerr << "error getting appdata path" << std::endl;
-		return -1;
-	}
-
-	std::wstring savePath(ppszPath);
-	savePath.append(LR"(\Whipseey\savedata\whipseey.sav)");
-
-	CSimpleIniA ini;
-
-	SI_Error rc = ini.LoadFile(savePath.data());
-	if(rc != SI_Error::SI_OK)
-	{
-		std::cerr << "load save failed, not si-ok" << std::endl;
-		return -1;
-	}
-
-	// CSimpleIniA::TNamesDepend sections;
-	// ini.GetAllSections(sections);
-	// for(auto& entryS : sections)
-	// {
-	// 	std::cout << "Line: " << entryS.nOrder << " Section: " << entryS.pItem << std::endl;
-	//
-	// 	CSimpleIniA::TNamesDepend keys;
-	// 	ini.GetAllKeys(entryS.pItem, keys);
-	// 	for(auto& entryK : keys)
-	// 	{
-	// 		std::cout << "  Line: " << entryK.nOrder << " Key: " << entryK.pItem << " Value: "
-	// 			<< ini.GetValue(entryS.pItem, entryK.pItem, nullptr, nullptr) << std::endl;
-	// 	}
-	// }
-
-	//! does not work with "" double moon = ini.GetDoubleValue("file1", "moon");
+	int screenWidth = 800;
+	int screenHeight = 600;
 	
-	std::wstring wchrBuffer;
-	DWORD bufferSizeBytes = 60 * sizeof(wchar_t);
-	wchrBuffer.reserve(bufferSizeBytes / sizeof(wchar_t));
+	SetConfigFlags(FLAG_WINDOW_UNDECORATED);
+	InitWindow(screenWidth, screenHeight, "raygui - portable window");
 
-	LSTATUS st = RegGetValueW(HKEY_LOCAL_MACHINE, LR"(SOFTWARE\WOW6432Node\Valve\Steam\)", L"InstallPath", RRF_RT_REG_SZ, NULL, wchrBuffer.data(), &bufferSizeBytes);
-	if(st == ERROR_MORE_DATA)
+	Vector2 mousePosition{ 0 };
+	Vector2 windowPosition = GetWindowPosition();//{ 500, 200 };
+	Vector2 panOffset = mousePosition;
+	bool dragWindow = false;
+	
+	// SetWindowPosition(windowPosition.x, windowPosition.y);
+	
+	bool exitWindow = false;
+	
+	SetTargetFPS(30);
+
+	while (!exitWindow && !WindowShouldClose())
 	{
-		wchrBuffer.reserve(bufferSizeBytes / sizeof(wchar_t));
-		st = RegGetValueW(HKEY_LOCAL_MACHINE, LR"(SOFTWARE\WOW6432Node\Valve\Steam\)", L"InstallPath", RRF_RT_REG_SZ, NULL, wchrBuffer.data(), &bufferSizeBytes);
-	}
-	if(st != ERROR_SUCCESS)
-	{
-		std::cerr << "reg read failed" << std::endl;
-		return -1;
-	}
-
-	std::wstring librariesVdf(wchrBuffer.data(), bufferSizeBytes / sizeof(wchar_t) - 1);
-
-	using namespace tyti;
-
-	librariesVdf.append(LR"(\steamapps\libraryfolders.vdf)");
-	std::ifstream vdf(librariesVdf);
-	auto root = vdf::read(vdf);
-	if(root.name == "LibraryFolders")
-	{
-		for(auto& attrib : root.attribs)
+		mousePosition = GetMousePosition();
+		
+		if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
 		{
-			if(std::isdigit(attrib.first.front()))
+			if (CheckCollisionPointRec(mousePosition, Rectangle{ 0, 0, (float)screenWidth, 20 }))
 			{
-				std::cout << attrib.first << " : " << attrib.second << std::endl;
+				dragWindow = true;
+				panOffset = mousePosition;
 			}
 		}
+
+		if (dragWindow)
+		{			
+			windowPosition.x += (mousePosition.x - panOffset.x);
+			windowPosition.y += (mousePosition.y - panOffset.y);
+			
+			if (IsMouseButtonReleased(MOUSE_LEFT_BUTTON)) dragWindow = false;
+
+			SetWindowPosition(windowPosition.x, windowPosition.y);
+		}
+
+		BeginDrawing();
+
+			ClearBackground(RAYWHITE);
+
+			exitWindow = GuiWindowBox(Rectangle{ 0, 0, (float)screenWidth, (float)screenHeight }, "PORTABLE WINDOW");
+			 ;
+			DrawText(FormatText("Mouse Position: [ %.0f, %.0f ]", mousePosition.x, mousePosition.y), 10, 40, 10, DARKGRAY);
+
+		EndDrawing();
 	}
+
+	CloseWindow();
 
 	return 0;
 }
