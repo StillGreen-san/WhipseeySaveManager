454a49405ef0bacd2914992d42ae92cb0b483767
StillGreen-san <40620628+StillGreen-san@users.noreply.github.com>
2024-08-06 09:43:40 +0200

    add file elements to gui

--- src/gui.rs
+++ src/gui.rs
@@ -1,8 +1,10 @@
+use iced::widget::Column;
 use iced::{Application, Command, Element, Renderer};
 use iced_aw::{TabLabel, Tabs};
 use std::path::PathBuf;
 
 use crate::gui::about::About;
+use crate::gui::file::DisplayStrings;
 
 pub mod about;
 pub mod cheats;
@@ -11,7 +13,7 @@ pub mod files;
 pub mod options;
 
 #[derive(Debug, Default, Clone, Copy, PartialEq, Eq)]
-enum TabId {
+pub enum TabId {
     #[default]
     About,
 }
@@ -32,10 +34,11 @@ pub enum Message {
     Reload(FileId, PathBuf),
 }
 
-#[derive(Default)]
 pub struct Gui {
     active_tab: TabId,
     about: About,
+    save: file::File,
+    bfs: file::File,
 }
 
 type Theme = iced::Theme;
@@ -47,7 +50,29 @@ impl Application for Gui {
     type Flags = ();
 
     fn new(_flags: Self::Flags) -> (Self, Command<Self::Message>) {
-        (Default::default(), Command::none())
+        let save_strings = DisplayStrings {
+            placeholder: "save game",
+            open: "...",
+            save: "save",
+            reload: "reload",
+            dialog_title: "save game",
+            dialog_filter_save: "save",
+            dialog_filter_all: "all",
+        }; // TODO localize
+        let bfs_strings = DisplayStrings {
+            placeholder: "bfs settings",
+            dialog_title: "bfs settings",
+            ..save_strings
+        };
+        (
+            Self {
+                active_tab: Default::default(),
+                about: Default::default(),
+                save: file::File::new(FileId::Save, save_strings),
+                bfs: file::File::new(FileId::Bfs, bfs_strings),
+            },
+            Command::none(),
+        )
     }
 
     fn title(&self) -> String {
@@ -61,14 +86,25 @@ impl Application for Gui {
                 Command::none()
             }
             Message::About(message) => self.about.update(message),
-            Message::File(id, message) => todo!(),
+            Message::File(id, message) => match id {
+                FileId::Save => self.save.update(message),
+                FileId::Bfs => self.bfs.update(message),
+            },
+            Message::Save(_, _) => todo!(),
+            Message::Open(_, _) => todo!(),
+            Message::Reload(_, _) => todo!(),
         }
     }
 
     fn view(&self) -> Element<'_, Self::Message, Self::Theme, Renderer> {
-        Tabs::new(Message::TabSelected)
-            .set_active_tab(&self.active_tab)
-            .push(TabId::About, self.about.tab_label(), self.about.view())
+        Column::new()
+            .push(self.save.view())
+            .push(self.bfs.view())
+            .push(
+                Tabs::new(Message::TabSelected)
+                    .set_active_tab(&self.active_tab)
+                    .push(TabId::About, self.about.tab_label(), self.about.view()),
+            )
             .into()
     }
 }
