96dd39668414dc428e6ec3a3a49927dd9539cba8
StillGreen-san <40620628+stillgreen-san@users.noreply.github.com>
2024-08-24 22:01:37 +0200

    ini impls for options

--- src/data/options.rs
+++ src/data/options.rs
@@ -1,6 +1,11 @@
+use crate::data::{IniKeyStr, IniSectionStr};
+use crate::{data, ini_impl};
+use ini::Properties;
+use num_enum::{IntoPrimitive, TryFromPrimitive};
 use strum::{Display, VariantArray};
 
-#[derive(Clone, Debug, Default, Display, VariantArray)]
+#[derive(Clone, Debug, Default, Display, VariantArray, TryFromPrimitive, IntoPrimitive)]
+#[repr(u8)]
 pub enum Language {
     #[default]
     English = 0,
@@ -14,30 +19,38 @@ pub enum Language {
     Chinese = 8,
     Portuguese = 9,
 }
+ini_impl!(Language, Options, "language");
 
-#[derive(Clone, Debug, Default, Display, VariantArray)]
+#[derive(Clone, Debug, Default, Display, VariantArray, TryFromPrimitive, IntoPrimitive)]
+#[repr(u8)]
 pub enum Scale {
     #[default]
     R768x432 = 2,
     R1152x648 = 3,
     R1536x864 = 4,
 }
+ini_impl!(Scale, Options, "scale");
 
-#[derive(Clone, Debug, Default, Display, VariantArray)]
+#[derive(Clone, Debug, Default, Display, VariantArray, TryFromPrimitive, IntoPrimitive)]
+#[repr(u8)]
 pub enum Fullscreen {
     Disabled = 0,
     #[default]
     Enabled = 1,
 }
+ini_impl!(Fullscreen, Options, "fullscreen");
 
-#[derive(Clone, Debug, Default, Display, VariantArray)]
+#[derive(Clone, Debug, Default, Display, VariantArray, TryFromPrimitive, IntoPrimitive)]
+#[repr(u8)]
 pub enum LeftHanded {
     Disabled = 0,
     #[default]
     Enabled = 1,
 }
+ini_impl!(LeftHanded, Options, "left_handed");
 
-#[derive(Clone, Debug, Default, Display, VariantArray)]
+#[derive(Clone, Debug, Default, Display, VariantArray, TryFromPrimitive, IntoPrimitive)]
+#[repr(u8)]
 pub enum SoundVolume {
     V0 = 0,
     V10 = 10,
@@ -52,15 +65,19 @@ pub enum SoundVolume {
     #[default]
     V100 = 100,
 }
+ini_impl!(SoundVolume, Options, "sound_volume", 100.0);
 
-#[derive(Clone, Debug, Default, Display, VariantArray)]
+#[derive(Clone, Debug, Default, Display, VariantArray, TryFromPrimitive, IntoPrimitive)]
+#[repr(u8)]
 pub enum SoundToggle {
     Disabled = 0,
     #[default]
     Enabled = 1,
 }
+ini_impl!(SoundToggle, Options, "sound_toggle");
 
-#[derive(Clone, Debug, Default, Display, VariantArray)]
+#[derive(Clone, Debug, Default, Display, VariantArray, TryFromPrimitive, IntoPrimitive)]
+#[repr(u8)]
 pub enum MusicVolume {
     V0 = 0,
     V10 = 10,
@@ -75,13 +92,16 @@ pub enum MusicVolume {
     #[default]
     V100 = 100,
 }
+ini_impl!(MusicVolume, Options, "music_volume", 100.0);
 
-#[derive(Clone, Debug, Default, Display, VariantArray)]
+#[derive(Clone, Debug, Default, Display, VariantArray, TryFromPrimitive, IntoPrimitive)]
+#[repr(u8)]
 pub enum MusicToggle {
     Disabled = 0,
     #[default]
     Enabled = 1,
 }
+ini_impl!(MusicToggle, Options, "music_toggle");
 
 #[derive(Clone, Default)]
 pub struct Options {
@@ -93,4 +113,40 @@ pub struct Options {
     pub sound_toggle: SoundToggle,
     pub music_volume: MusicVolume,
     pub music_toggle: MusicToggle,
+}
+
+impl IniSectionStr for Options {
+    const INI_SECTION_STR: &'static str = "options";
+}
+
+impl TryFrom<&Properties> for Options {
+    type Error = data::Error;
+
+    fn try_from(value: &Properties) -> Result<Self, Self::Error> {
+        Ok(Self {
+            language: value.try_into()?,
+            scale: value.try_into()?,
+            fullscreen: value.try_into()?,
+            left_handed: value.try_into()?,
+            sound_volume: value.try_into()?,
+            sound_toggle: value.try_into()?,
+            music_volume: value.try_into()?,
+            music_toggle: value.try_into()?,
+        })
+    }
+}
+
+impl From<Options> for Properties {
+    fn from(value: Options) -> Self {
+        let mut props = Properties::new();
+        props.insert(value.language.ini_key_str(), value.language);
+        props.insert(value.scale.ini_key_str(), value.scale);
+        props.insert(value.fullscreen.ini_key_str(), value.fullscreen);
+        props.insert(value.left_handed.ini_key_str(), value.left_handed);
+        props.insert(value.sound_volume.ini_key_str(), value.sound_volume);
+        props.insert(value.sound_toggle.ini_key_str(), value.sound_toggle);
+        props.insert(value.music_volume.ini_key_str(), value.music_volume);
+        props.insert(value.music_toggle.ini_key_str(), value.music_toggle);
+        props
+    }
 }
