d5487ee758b5d0d8ec5197991b9d69f9666e5a41
StillGreen-san <40620628+StillGreen-san@users.noreply.github.com>
2024-08-26 14:04:10 +0200

    add for_each_window_mut

--- src/util.rs
+++ src/util.rs
@@ -3,3 +3,12 @@ pub mod file;
 pub use file::load_ini_file;
 pub use file::write_ini_file;
 pub use file::write_ini_file_padded;
+
+pub fn for_each_window_mut<T, F>(slice: &mut [T], size: usize, mut function: F)
+where
+    F: FnMut(&mut [T]),
+{
+    for start in 0..=(slice.len().saturating_sub(size)) {
+        function(&mut slice[start..][..size]);
+    }
+}
