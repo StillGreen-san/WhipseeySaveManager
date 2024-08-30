9ada618e7221f1ac1274b5c4929b3f1992af31e1
StillGreen-san <40620628+stillgreen-san@users.noreply.github.com>
2024-08-30 15:19:45 +0200

    copy assert_matches from nightly

--- src/util/test.rs
+++ src/util/test.rs
@@ -9,3 +9,59 @@ pub mod ini {
     pub const LENIENT_VALUES: &str = include_str!("test/lenient-values.ini");
     pub const VALID: &str = include_str!("test/valid.ini");
 }
+
+// copied from nightly https://doc.rust-lang.org/src/core/macros/mod.rs.html#154-179
+#[macro_export]
+macro_rules! assert_matches {
+    ($left:expr, $(|)? $( $pattern:pat_param )|+ $( if $guard: expr )? $(,)?) => {
+        match $left {
+            $( $pattern )|+ $( if $guard )? => {}
+            ref left_val => {
+                $crate::util::test::assert_matches_failed(
+                    left_val,
+                    std::stringify!($($pattern)|+ $(if $guard)?),
+                    std::option::Option::None
+                );
+            }
+        }
+    };
+    ($left:expr, $(|)? $( $pattern:pat_param )|+ $( if $guard: expr )?, $($arg:tt)+) => {
+        match $left {
+            $( $pattern )|+ $( if $guard )? => {}
+            ref left_val => {
+                $crate::util::test::assert_matches_failed(
+                    left_val,
+                    std::stringify!($($pattern)|+ $(if $guard)?),
+                    std::option::Option::Some(std::format_args!($($arg)+))
+                );
+            }
+        }
+    };
+}
+// copied from nightly https://doc.rust-lang.org/src/core/panicking.rs.html#371-414
+pub fn assert_matches_failed<T: fmt::Debug + ?Sized>(
+    left: &T,
+    right: &str,
+    args: Option<fmt::Arguments<'_>>,
+) -> ! {
+    struct Pattern<'a>(&'a str);
+    impl fmt::Debug for Pattern<'_> {
+        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+            f.write_str(self.0)
+        }
+    }
+    let right = &Pattern(right);
+
+    match args {
+        Some(args) => panic!(
+            r#"assertion `left matches right` failed: {args}
+  left: {left:?}
+ right: {right:?}"#
+        ),
+        None => panic!(
+            r#"assertion `left matches right` failed
+  left: {left:?}
+ right: {right:?}"#
+        ),
+    };
+}
