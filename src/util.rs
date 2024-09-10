7f1f8c3764eed0ca7e031ad4e39de5796deb2c55
StillGreen-san <40620628+StillGreen-san@users.noreply.github.com>
2024-09-10 12:50:42 +0200

    add test & doc to for_each_window_mut

--- src/util.rs
+++ src/util.rs
@@ -11,11 +11,46 @@ pub use file::write_ini_file;
 pub use file::write_ini_file_padded;
 pub use file::LocateError;
 
+/// Executes `function` for all contiguous windows of length `size` in `slice`. The windows overlap.
+/// If the slice is shorter than `size`, the function is not called.
+///
+/// free function alternative to the [std::slice::Windows] iterator,
+/// taken from `kpreid` https://internals.rust-lang.org/t/a-windows-mut-method-on-slice/16941/11
+///
+/// # Examples
+///
+/// ```
+/// # use whipseey_save_manager::util::for_each_window_mut;
+/// let mut slice = [1, 2, 3, 2, 3, 1];
+/// for_each_window_mut(&mut slice, 3, |win| {
+///     if win == [1, 2, 3] {
+///         win[2] = 1;
+///     }
+/// });
+/// assert_eq!(slice, [1, 2, 1, 2, 1, 1]);
+/// ```
+///
 pub fn for_each_window_mut<T, F>(slice: &mut [T], size: usize, mut function: F)
 where
     F: FnMut(&mut [T]),
 {
     for start in 0..=slice.len().saturating_sub(size) {
         function(&mut slice[start..][..size]);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn for_each_window_mut_duplicate_new_lines() {
+        let mut data = Vec::from(b"line\n\nline\nline\n\nline\n");
+        for_each_window_mut(&mut data, 2, |window| {
+            if window == b"\n\n" {
+                window[1] = 0;
+            }
+        });
+        assert_eq!(data, b"line\n\0line\nline\n\0line\n");
     }
 }
