8a29dd40b739c729fbe1cf883f89a1c2409c5702
StillGreen-san <40620628+StillGreen-san@users.noreply.github.com>
2024-09-12 14:09:55 +0200

    use winres in build.rs to set version info for windows executables

---
+++ build.rs
@@ -0,0 +1,18 @@
+#[cfg(windows)]
+extern crate winres;
+
+fn main() {
+    #[cfg(windows)]
+    windows();
+}
+
+#[cfg(windows)]
+fn windows() {
+    const LID_ENGLISH_US: u16 = 0x0409;
+    const VS_FF_PRERELEASE: u64 = 0x00000002;
+    winres::WindowsResource::new()
+        .set_language(LID_ENGLISH_US)
+        .set_version_info(winres::VersionInfo::FILEFLAGS, VS_FF_PRERELEASE)
+        .compile()
+        .unwrap();
+}
