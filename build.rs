8a97105bb317f39fc3a735878f694d7b278971a1
StillGreen-san <40620628+StillGreen-san@users.noreply.github.com>
2024-09-23 19:28:07 +0200

    update build settings for v0.12

--- build.rs
+++ build.rs
@@ -9,11 +9,17 @@ fn main() {
 #[cfg(windows)]
 fn windows() {
     const LID_ENGLISH_US: u16 = 0x0409;
-    const VS_FF_PRERELEASE: u64 = 0x00000002;
+    const VS_FF_PRERELEASE: u64 = 0x000002;
+    const VS_FF_NONE: u64 = 0x000000;
+    let file_flag = if std::env::var("CARGO_PKG_VERSION_PRE").unwrap().is_empty() {
+        VS_FF_NONE
+    } else {
+        VS_FF_PRERELEASE
+    };
     winres::WindowsResource::new()
         .set_icon("res/icon.ico")
         .set_language(LID_ENGLISH_US)
-        .set_version_info(winres::VersionInfo::FILEFLAGS, VS_FF_PRERELEASE)
+        .set_version_info(winres::VersionInfo::FILEFLAGS, file_flag)
         .compile()
         .expect("resource compiler failure");
 }
